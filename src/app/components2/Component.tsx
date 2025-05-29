65dc1538f538296382509678d00a2769c06fceff
emmeiwhite <emmeiwhite@gmail.com>
2025-05-29 14:19:51 +0530

    error: The return type of an async function or method must be the global Promise<T> type.

--- src/app/components2/Component.tsx
+++ src/app/components2/Component.tsx
@@ -1,6 +1,12 @@
 type ComponentProps = { name: string; age: number }
 
 const Component = ({ name, age }: ComponentProps) => {
+  // async-function example
+
+  async function getData(): string {
+    return 'data fetched successfully'
+  }
+
   return (
     <div>
       <strong>Name : </strong> {name}
